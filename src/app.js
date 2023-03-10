b66b9384ee10e15b3d4aa3975a6ee40a6393d34a
Crhistian Alexander Garcia Urbano <garcia.crhistian@correounivalle.edu.co>
2023-03-10 20:05:21 +0000

    my first and unique comit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const pronouns = ["the", "our"];
+const adjs = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+
+const getPermutations = (pronouns, adjs, nouns) => {
+  for (const pronoun of pronouns) {
+    for (const adj of adjs) {
+      for (const noun of nouns) {
+        console.log(`${pronoun}${adj}${noun}.com`);
+      }
+    }
+  }
 };
+
+getPermutations(pronouns, adjs, nouns);
